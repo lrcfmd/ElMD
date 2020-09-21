d2c5df64eb705b49ee3009665f0907bea1160954
Hargreaves <sgchargr@liverpool.ac.uk>
2020-09-21 17:17:00 +0100

    updated version number

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'ElMD',        
   packages = ['ElMD'],  
-  version = '0.1',      
+  version = '0.2.1',      
   license='GPL3',       
   description = 'An implementation of the Element movers distance for chemical similarity of ionic compositions',  
   author = 'Cameron Hagreaves',              
   author_email = 'cameron.h@rgreaves.me.uk', 
   url = 'https://github.com/lrcfmd/ElMD/',   
-  download_url = 'https://github.com/lrcfmd/ElMD/archive/v0.1.tar.gz',    
+  download_url = 'https://github.com/lrcfmd/ElMD/archive/v0.2.1.tar.gz',    
   keywords = ['ChemInformatics', 'Materials Science', 'Machine Learning', 'Materials Representation'],   
   install_requires=[            
           'numpy',
