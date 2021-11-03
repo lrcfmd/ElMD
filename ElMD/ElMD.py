fbb6c2f7abf8f2861913b14dc1d326822237ab69
SurgeArrester <cameron.h@rgreaves.me.uk>
2021-11-03 14:48:52 +0000

    Fixed multiplier for exported EMD function

--- ElMD/ElMD.py
+++ ElMD/ElMD.py
@@ -44,8 +44,8 @@ from functools import lru_cache
 def main():
     import time 
     ts = time.time()
-    x = ElMD("Li0.167 Al0.167 H0.667", metric="mat2vec")
-    y = ElMD("NaCl", metric="mat2vec")
+    x = ElMD("LiCl", metric="mod_petti")
+    y = ElMD("NaCl", metric="mod_petti")
     # z = ElMD("Zr3AlN", metric="atomic")
 
     print(x.elmd(y))
@@ -116,7 +116,7 @@ def elmd(comp1, comp2, metric="mod_petti"):
     sink_demands = sink_demands[np.where(sink_demands > 0)[0]]
 
     # Perform a floating point conversion to ints to ensure algorithm terminates
-    network_costs = np.array([[np.linalg.norm(x - y) * 1000000 for x in sink_labels] for y in source_labels], dtype=np.int64) 
+    network_costs = np.array([[np.linalg.norm(x - y) for x in sink_labels] for y in source_labels], dtype=np.int64) 
 
     return EMD(source_demands, sink_demands, network_costs)
 
@@ -788,6 +788,7 @@ def network_simplex(source_demands, sink_demands, network_costs):
     # FP conversion error correction
     source_sum = np.sum(source_d_int)
     sink_sum = np.sum(sink_d_int)
+    
     if  source_sum < sink_sum:
         source_ind = np.argmax(source_d_int)
         source_d_int[source_ind] += sink_sum - source_sum
@@ -832,6 +833,8 @@ def network_simplex(source_demands, sink_demands, network_costs):
 
     # Set a suitably high integer for infinity
     faux_inf = 3 * np.max(np.array((np.sum(network_capac.astype(np.int64)), np.sum(np.absolute(network_costs)), np.max(np.absolute(demands))), dtype=np.int64))
+
+    network_costs = network_costs * fp_multiplier
 
     # Add the costs and capacities to the dummy nodes
     costs = np.concatenate((network_costs, np.ones(nodes.shape[0]) * faux_inf)).astype(np.int64)
