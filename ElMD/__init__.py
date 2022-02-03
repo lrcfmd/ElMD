73bba932b6aab9fa439a6684ec04a13db89c90c3
SurgeArrester <cameron.h@rgreaves.me.uk>
2022-02-03 13:08:03 +0000

    Fixed elmd method import

--- ElMD/__init__.py
+++ ElMD/__init__.py
@@ -1,3 +1,3 @@
 """Implementation of Element movers distance for chemical similarity compositions."""
-__version__ = "0.4.20"
-from ElMD.ElMD import EMD, ElMD 
+__version__ = "0.4.21"
+from ElMD.ElMD import EMD, elmd, ElMD 
